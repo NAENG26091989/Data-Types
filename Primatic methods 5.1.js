8b07e7b05052c22ab3d30dd029e26ec84eb8e07b
Qobil Abdurahmonov <119861600+NAENG26091989@users.noreply.github.com>
2023-02-26 05:11:04 +0500

    endured change
    endured change

--- Primatic methods 5.1.js
+++ Primatic methods 5.1.js
@@ -13,3 +13,28 @@
 // alert( typeof new Number(0) ); // "object"
 
 // null/undefined не имеют методов
+// Есть 7 методы примативов:
+//  string,
+//  number,
+//  boolean,
+//  symbol, 
+//  null, 
+//  undefined,
+//  bigint.
+
+
+
+const isProgrammer= true;
+const name = 'Qobil';
+const age=26;
+let x
+// console.log(typeof isProgrammer); //boolean
+// console.log(typeof name); //string
+// console.log( typeof age ); //number
+console.log(typeof x); //underfined
+
+
+
+
+// let str = "Привет";
+// alert( str.toUpperCase() ); // пример
